e27dfd2e6bdccc5a60c3ec3f4b6e8c6365008e12
ly <850362834@qq.com>
2021-05-30 14:39:51 +0800

    2021.5.30-11:32 开发栅格布局组件

---
+++ src/components/render-engine/extend-engine/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script lang="ts">
+import {defineComponent, PropType} from 'vue';
+export default defineComponent({
+  name: 'components',
+  props: {
+
+  },
+  data(){
+    return {
+
+    }
+  },
+  components:{
+
+  },
+  methods: {
+
+  },
+  mounted(){
+
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+<style lang="less">
+
+</style>
+
---
+++ src/components/render-engine/extend-engine/row/row.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-row>
+    <el-col></el-col>
+  </el-row>
+</template>
+<script lang="ts">
+import {defineComponent, PropType} from 'vue';
+export default defineComponent({
+  name: 'components',
+  props: {
+
+  },
+  data(){
+    return {
+
+    }
+  },
+  components:{
+
+  },
+  methods: {
+
+  },
+  mounted(){
+
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+<style lang="less">
+
+</style>
+
