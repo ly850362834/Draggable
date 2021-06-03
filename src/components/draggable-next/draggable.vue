6e16b1cb3fd6ee494d4657dacb320b0636044a0a
ly <850362834@qq.com>
2021-06-03 21:33:02 +0800

    1.0: 创建Vuedraggable 文件夹

---
+++ src/components/draggable-next/draggable.vue
@@ -0,0 +1,66 @@
+<template>
+  <div :id="renderId">
+    <template v-if="views">
+      <slot name="content">
+
+      </slot>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+// import Sortable from 'sortablejs';
+import {defineComponent, PropType,onBeforeUnmount} from 'vue';
+// import eventBus from "@/assets/api/eventBus.ts";
+
+export default defineComponent({
+  // setup() {
+  //   function changeMenu(){
+  //     eventBus.$emit("change-menu");
+  //   }
+  //   return {changeMenu};
+  // },
+  name: 'draggable',
+  data(){
+    return {
+      views:true,
+      renderId:`${new Date().valueOf()}`,
+    }
+  },
+  props: {
+    // id: {
+    //   type: String,
+    //   required: true
+    // },
+    // list: {
+    //   type: Array,
+    //   default: () => ([])
+    // },
+    // sortableObj: {
+    //   type: Object,
+    //   required: false
+    // },
+    // group: {
+    //   type: Object,
+    //   required: false,
+    //   default: () => ({
+    //     name: "itxst.com", pull: 'clone', put: false
+    //   })
+    // }
+  },
+  created() {
+
+  },
+  methods: {
+
+  },
+  mounted(){
+    this.registerDom();
+  },
+});
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+</style>
